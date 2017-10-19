482a0d3f106a41db06d026c0dca5dc29264a0b5d
conradmccarthy <cdm223@cornell.edu>
2017-10-19 00:05:34 -0400

    Add files via upload

---
+++ SBW_tests_both_encoders/PID.h
@@ -0,0 +1,26 @@
+#ifndef PID_h
+#define PID_h
+
+#include <SPI.h>
+#include <math.h>
+
+/*Define definite variables*/
+//PID
+#define PWM_front 8
+#define DIR 11
+#define K_p 5000 //200
+#define K_d -50 // -1.5 ish
+#define K_i 0
+
+/*Define functions*/
+//PID
+void PID_Controller(float, signed int, signed int, 
+  unsigned long, unsigned long, signed int, float, float);
+
+
+#endif //PID_h
+
+//Gameplan:
+//Try out different gains off ground for holding position (stationary testing)
+//Try out different gains ON ground (max 45 minutes) (step input)
+//if failed, try different gains OFF ground, tune (step input)
